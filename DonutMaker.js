c8196de05201fa0ec04be80f1902cf4d60cb1e73
BenjaminCorrigan <corriganbenjamin>
2022-03-12 21:11:19 -0500

    stuck on modals only 1 works

---
+++ DonutMaker.js
@@ -0,0 +1,84 @@
+export default class DonutMaker{
+
+    constructor(){
+        
+        this.DonutCount = 0;
+        this.AutoclickCount = 0;
+        this.AutoCost= 15;
+        this.MultiCost= 10;
+        this.MultiplierCount = 0;
+        this.AutoBuy = false;
+        this.MultiBuy = false;
+        this.reloadPage;
+    }
+    
+    click(){
+       //Make donuts and multiply autoclicker
+        if (this.MultiplierCount === 0){
+            this.DonutCount += 1;
+        }else{
+            this.DonutCount += Math.pow(1.2, this.MultiplierCount);
+        }
+        
+         
+        // if (this.DonutCount >= 100){
+        //     this.AutoclickCount = true;
+        // }
+        // if (this.DonutCount >= 10){
+        //     this.Multiplier = true;
+        // }
+
+        //buy buttons = true
+        if (this.DonutCount >= this.AutoCost) {
+            this.AutoBuy = true;
+        }
+        if (this.DonutCount >= this.MultiCost) {
+            this.MultiBuy = true;
+        }
+
+        console.log(this.DonutCount)
+
+    }
+
+    
+
+    BuyAutoClick(){
+
+        if (this.DonutCount >= this.AutoCost){
+            this.DonutCount -= this.AutoCost;
+            this.AutoclickCount += 1;
+        }
+        if (this.DonutCount < this.AutoCost){
+            this.AutoBuy = false;
+        }
+        // if (this.Autoclick === 0){
+        //     this.autoCost = false;
+        // }
+    }
+
+    BuyMultiClick(){
+
+        if (this.DonutCount >= this.MultiCost){
+            this.DonutCount -= this.MultiCost;
+            this.MultiplierCount += 1;
+        }
+        if (this.DonutCount < this.MultiCost){
+            this.MultiBuy = false;
+        }
+        if(this.donutCount >= this.MultiCost){
+            this.MultiplierCount++
+            this.donutCount -= this.MultiCost;
+            this.MultiCost = this.MultiCost * 1.1;
+        }
+    }   
+
+     reloadPage(){
+        // resetButton.onclick=
+        window.location.reload();
+
+        }
+    
+
+
+
+}
--- index.js
+++ index.js
@@ -1,2 +1,87 @@
-const container = (document.querySelector('.container').innerText =
-  'mmmmmmmmm doughnuts.... ahhhhhhh!');
+import DonutMaker from "./DonutMaker.js";
+
+const donutMaker = new DonutMaker()
+
+const container = document.querySelector('.container')
+
+const autoClickerBtn = document.querySelector('.Auto-Clicker')
+const donutMultiplierBtn = document.querySelector('.Donut-Multiplier')
+
+
+function updateView(){
+  autoClickerBtn.disabled = !donutMaker.AutoBuy;
+  donutMultiplierBtn.disabled = !donutMaker.MultiBuy;
+  donutCount.innerText = "You'r donut count: " + donutMaker.DonutCount.toFixed(1);
+  AutoclickerCount.innerText = "Auto Count:" + donutMaker.AutoclickCount.toFixed(1);
+  donutMultiplierCount.innerText = " Multi Pass " + donutMaker.MultiplierCount.toFixed(1);
+}
+
+const donutCount = document.querySelector('.DonutCount')
+const makeDonutsBtn = document.querySelector('.Make-Donuts')
+makeDonutsBtn.addEventListener("click", () =>{
+  donutMaker.click()
+  updateView()
+})
+
+
+const AutoclickerCount = document.querySelector('.AutoclickerCount')
+
+autoClickerBtn.addEventListener("click",()=>{
+  donutMaker.BuyAutoClick();
+  setInterval(clickBait, 1000);
+  setInterval(updateView, 1000);
+  updateView()
+})
+
+function clickBait() {
+  donutMaker.click()
+}
+
+const donutMultiplierCount = document.querySelector('.DonutMultiplierCount')
+donutMultiplierBtn.addEventListener("click", () => {
+donutMaker.BuyMultiClick();
+
+})
+
+
+updateView();
+
+let reseterButtoner = document.querySelector('.Reseter-Buttoner')
+reseterButtoner.addEventListener("click", () => {
+donutMaker.reloadPage();
+})
+// Get the modal
+var modal = document.getElementById("myModal1");
+// var modal = document.getElementById("myModal2");
+// var modal = document.getElementById("myModal3");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn1");
+// var btn = document.getElementById("myBtn2");
+// var btn = document.getElementById("myBtn3");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+  
+
+
+
+
+
